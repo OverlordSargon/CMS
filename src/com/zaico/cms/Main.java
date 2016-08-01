89f29a2c981f97abd66b9e335fb6171ee66f5970
Zaitsev Nikita <nzaitsev@t-systems.ru>
2016-08-01 18:06:34 +0300

    Initial commit

---
+++ src/com/zaico/cms/Main.java
@@ -0,0 +1,10 @@
+package com.zaico.cms;
+
+/**
+ * Created by nzaitsev on 01.08.2016.
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("HELllo");
+    }
+}
