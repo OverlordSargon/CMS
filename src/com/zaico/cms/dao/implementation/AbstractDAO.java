1aa70e0e629e5d1dfd60d6cdd4b23c84b5cebbff
nzaitsev <GJKB713DBNFVBYrf>
2016-08-11 12:29:36 +0300

    H2-4Create DAO: Abstract DAO now have EM.rollback, to prevent errors

--- src/com/zaico/cms/dao/implementation/AbstractDAO.java
+++ src/com/zaico/cms/dao/implementation/AbstractDAO.java
@@ -51,22 +51,33 @@ public abstract class AbstractDAO<T extends AbstractEntity> implements CommonDAO
      * @param t Entity type
      */
     public T create(T t) {
-        t.setCreatedAt(new Date());
-        t.setUpdatedAt(new Date());
-        em.getTransaction().begin();
-        em.persist(t);
-        em.getTransaction().commit();
+        try {
+            t.setCreatedAt(new Date());
+            t.setUpdatedAt(new Date());
+            em.getTransaction().begin();
+            em.persist(t);
+            em.getTransaction().commit();
+        }
+        catch ( Exception exp ) {
+            em.getTransaction().rollback();
+        }
         return t;
-    };
+    }
 
     /**
      * Get object by id
      */
     public T read(Long id) {
-        em.getTransaction().begin();
-        T resultRead = em.find(type,id);
-        em.getTransaction().commit();
-        return resultRead;
+        T result = null;
+        try {
+            em.getTransaction().begin();
+            result = em.find(type, id);
+            em.getTransaction().commit();
+        }
+        catch (Exception exp) {
+            em.getTransaction().rollback();
+        }
+        return result;
     }
 
     /**
@@ -74,11 +85,17 @@ public abstract class AbstractDAO<T extends AbstractEntity> implements CommonDAO
      * @param t Entity object
      */
     public T update(T t) {
-        t.setUpdatedAt(new Date());
-        em.getTransaction().begin();
-        T resultUpdate = em.merge(t);
-        em.getTransaction().commit();
-        return resultUpdate;
+        T result = null;
+        try {
+            t.setUpdatedAt(new Date());
+            em.getTransaction().begin();
+            result = em.merge(t);
+            em.getTransaction().commit();
+        }
+        catch (Exception exp) {
+            em.getTransaction().rollback();
+        }
+        return result;
     }
 
     /**
@@ -86,26 +103,44 @@ public abstract class AbstractDAO<T extends AbstractEntity> implements CommonDAO
      * @param t Entity, we want to delete
      */
     public void delete(T t) {
-        em.getTransaction().begin();
-        em.remove(t);
-        em.getTransaction().commit();
+        try {
+            em.getTransaction().begin();
+            em.remove(t);
+            em.getTransaction().commit();
+        }
+            catch (Exception exp) {
+            em.getTransaction().rollback();
+        }
     }
 
     /**
      * Get all users
      * @return users like result list
      */
-    public List<T> getAll(){
-        List<T> resultGetAll = em.createNamedQuery(entityName+".getAll").getResultList();
-        return resultGetAll;
+    public List<T> getAll() {
+        List<T> result = null;
+        try {
+            em.getTransaction().begin();
+            result = em.createNamedQuery(entityName+".getAll").getResultList();
+            em.getTransaction().commit();
+        }
+        catch (Exception exp) {
+            em.getTransaction().rollback();
+        }
+        return result;
     }
 
     /**
      * Deletes all existing users
      */
     public void deleteAll() {
-        em.getTransaction().begin();
-        em.createNamedQuery(entityName+".deleteAll").executeUpdate();
-        em.getTransaction().commit();
+        try {
+            em.getTransaction().begin();
+            em.createNamedQuery(entityName+".deleteAll").executeUpdate();
+            em.getTransaction().commit();
+        }
+        catch (Exception exp) {
+            em.getTransaction().rollback();
+        }
     }
 }
