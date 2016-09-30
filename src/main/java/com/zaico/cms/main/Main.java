ed34e6a0272e8f8c7009f7b40550a89c384dd224
nzaitsev <GJKB713DBNFVBYrf>
2016-09-30 17:57:01 +0300

    Catch order error, creates user

--- src/main/java/com/zaico/cms/main/Main.java
+++ src/main/java/com/zaico/cms/main/Main.java
@@ -28,7 +28,7 @@ public class Main {
     public static Logger logger = Logger.getLogger(HelloWorld.class);
 
     public static void main(String[] args)  throws ExceptionCMS, SQLException, ClassNotFoundException,InterruptedException , IllegalArgumentException,
-        IllegalAccessException, ParseException {
+            IllegalAccessException, ParseException {
 //        OrderService orderService = FactoryService.getOrderServiceInstance();
 //        WorkerService workerService = FactoryService.getWorkerServiceInstance();
 //        SkillService skillService = FactoryService.getSkillServiceInstance();
