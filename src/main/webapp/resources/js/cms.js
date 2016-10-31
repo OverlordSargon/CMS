670bb71314852eedf1f02c37673c2031043b6255
nzaitsev <GJKB713DBNFVBYrf>
2016-10-31 13:28:05 +0300

    Spring. Style

---
+++ src/main/webapp/resources/js/cms.js
@@ -0,0 +1,11 @@
+/**
+ * Created by ZAITNIK on 29.10.2016.
+ */
+
+// Options
+var options = {
+    offset: 40
+};
+
+// Create a new instance of Headhesive.js and pass in some options
+var header = new Headhesive('header', options);
