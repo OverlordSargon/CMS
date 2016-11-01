9dd3c465293285ffbfc63fcf62dc8ca7683d0dc6
nzaitsev <GJKB713DBNFVBYrf>
2016-11-01 13:53:37 +0300

    Spring. Worker create model from jsp

---
+++ src/main/java/com/zaico/cms/controllers/worker/WorkerDates.java
@@ -0,0 +1,53 @@
+package com.zaico.cms.controllers.worker;
+
+/**
+ * Created by nzaitsev on 01.11.2016.
+ */
+public class WorkerDates {
+
+    String begindate;
+    String beginhour;
+    String enddate;
+    String endhour;
+    String breakstart;
+
+    public String getBegindate() {
+        return begindate;
+    }
+
+    public void setBegindate(String begindate) {
+        this.begindate = begindate;
+    }
+
+    public String getBeginhour() {
+        return beginhour;
+    }
+
+    public void setBeginhour(String beginhour) {
+        this.beginhour = beginhour;
+    }
+
+    public String getEnddate() {
+        return enddate;
+    }
+
+    public void setEnddate(String enddate) {
+        this.enddate = enddate;
+    }
+
+    public String getEndhour() {
+        return endhour;
+    }
+
+    public void setEndhour(String endhour) {
+        this.endhour = endhour;
+    }
+
+    public String getBreakstart() {
+        return breakstart;
+    }
+
+    public void setBreakstart(String breakstart) {
+        this.breakstart = breakstart;
+    }
+}
